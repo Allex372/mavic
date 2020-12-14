cc04e1dda7ca26fe32c00342e25f4c16e3bcf1df
Allex372 <62441924+Allex372@users.noreply.github.com>
2020-12-15 00:10:44 +0200

    hw5

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,77 @@
 import React, {Component} from 'react';
+import {NavLink} from "react-router-dom";
 
 class About extends Component {
     render() {
         return (
-            <div>
-                TEXT
-            </div>
+
+                <div className={'main'}>
+
+                    <header className={'header'}>
+
+                        <div className={'top-left'}>
+
+                            <div className={'pic'}></div>
+                            <div className={'header-text-style'}>
+                                MAVIC 2 PRO
+                            </div>
+                        </div>
+
+                        <div className={'top-center'}>
+                            <NavLink to={'/about'}>
+                                <div className={'header-text-style'}>
+                                    О ДРОНЕ
+                                </div>
+                            </NavLink>
+
+
+                            <div className={'header-text-style'}>
+                                <NavLink to='/info'>
+                                    <div className={'header-text-style'}>
+                                        ПРЕИМУЩЕСТВА
+                                    </div>
+                                </NavLink>
+                            </div>
+                            <div className={'header-text-style'}>
+                                <NavLink to='/'>
+                                    <div className={'header-text-style'}>
+                                        ХАРАКТЕРИСТИКИ
+                                    </div>
+                                </NavLink>
+                            </div>
+                            <div className={'header-text-style'}>
+                                <NavLink to='/'>
+                                    <div className={'header-text-style'}>
+                                        ВОПРОСЫ
+                                    </div>
+                                </NavLink>
+                            </div>
+                            <div className={'header-text-style'}>
+                                <NavLink to='/'>
+                                    <div className={'header-text-style'}>
+                                        КОНТАКТЫ
+                                    </div>
+                                </NavLink>
+                            </div>
+                        </div>
+
+                        <div className={'top-right'}>
+                            <button className={'btn-top'}>
+                                <div className={'header-text-style'}>
+                                    Купить
+                                </div>
+                            </button>
+                            <NavLink to='/'>
+                                <div className={'header-text-style'}>
+                                    0717171711
+                                </div>
+                            </NavLink>
+                        </div>
+
+                    </header>
+
+
+                </div>
         );
     }
 }
